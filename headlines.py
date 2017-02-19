61cc6e0cab122ac930c6e70dc58b3546e0945271
RunSi <sihart@cisco.com>
2017-02-19 09:49:09 +0000

    first commit

---
+++ headlines.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+
+import feedparser
+from flask import Flask
+
+app = Flask(__name__)
+
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
+@app.route("/")
+@app.route("/<publication>")
+
+# def bbc():
+#     return get_news('bbc')
+
+# @app.route("/cnn")
+# def cnn():
+#     return get_news('cnn')
+
+# @app.route("/fox")
+# def fox():
+#     return get_news("fox")
+
+
+def get_news(publication='bbc'):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    first_article = feed['entries'][0]
+    return '''<html>
+    <body>
+        <h1> {0} Headlines </h1>
+        <b>{1}</b> <br/>
+        <i>{2}</i> <br/>
+        <p>{3}</p> <br/>
+    </body>
+    </html>'''.format(publication.upper(),first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
